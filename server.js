7e27cf2f6d7755b3f4f7c46935b9c029aa240e98
Guilherme Fermino <guilhermesantos026@gmail.com>
2020-03-24 03:02:12 +0000

    config express

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const express = require('express');
-
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function() {
-    
+
 });
 
 app.get('/', function(req, resp) {
